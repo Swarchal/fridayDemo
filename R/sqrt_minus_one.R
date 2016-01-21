786480161d0f2df858e929dacb4fa53017595264
Swarchal <S.Warchal@sms.ed.ac.uk>
2016-01-21 23:04:09 +0000

    add second function

---
+++ R/sqrt_minus_one.R
@@ -0,0 +1,20 @@
+#' square root minus 1
+#'
+#' Given a number, this function will take the find the square root minus one
+#'
+#' @param x number
+#' @return out number
+#' @export
+#'
+#' @examples
+#' vals <- rnorm(10)
+#' sqrt_minus_one(vals)
+
+
+
+sqrt_minus_one <- function(x){
+  out <- sqrt(x) - 1
+  # out <- sqrt(x - 1)
+
+  return(out)
+}
--- R/square_plus_one.R
+++ R/square_plus_one.R
@@ -1,13 +1,15 @@
-#' add_one
+#' add_one to vector
 #'
 #' Adds 1 to each element of a vector
 #'
 #' @param x a numerical vector
-#' @return out a numerical vector 
+#' @return out a numerical vector
+#'
+#' @export
 #'
 #' @examples
 #' vals <- seq(1, 10, 1)
-#' add_one(vals)
+#' square_add_one(vals)
 
 square_plus_one <- function(x){
 	out <- x^2 + 1
